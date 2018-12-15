caeeaede09c47e43fc6b32cf833ec1a40d76800c
sirfcsn <sirfcsn@gmail.com>
2018-12-15 20:46:27 +0900

    feat: Add posts' get, post, delete, put, patch feat in posts.ctrl.js

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,20 +1,15 @@
 const Router = require('koa-router');
+const postsCtrl = require('./posts.ctrl');
 
 const posts = new Router();
 
-const printInfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params
-  };
-};
-
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+// 라우트 처리 함수만 모아 놓은 파일을 컨트롤러
+// 컨트롤러에서는 백엔드 기능을 구현
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+posts.get('/:id', postsCtrl.read);
+posts.delete('/:id', postsCtrl.remove);
+posts.put('/:id', postsCtrl.replace);
+posts.patch('/:id', postsCtrl.update);
 
 module.exports = posts;
