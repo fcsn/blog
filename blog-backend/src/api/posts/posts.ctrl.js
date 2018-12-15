93234fbae3116069f956eeb01a3f8a63ab4028f5
sirfcsn <sirfcsn@gmail.com>
2018-12-15 21:39:09 +0900

    feat: Connect server to Mongodb and Add write controller

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,100 +1,28 @@
-let postId = 1;
+const Post = require('models/post');
 
-const posts = [
-  {
-    id: 1,
-    title: '제목',
-    body: '내용'
+exports.write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+
+  const post = new Post({
+    title, body, tags
+  });
+
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(e, 500);
   }
-];
-
-// POST /api/posts
-exports.write = (ctx) => {
-  const {
-    title,
-    body
-  } = ctx.request.body;
-
-  postId += 1;
-
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
 };
 
-// GET /api/posts
 exports.list = (ctx) => {
-  ctx.body = posts;
 };
 
-// GET /api/posts/:id
 exports.read = (ctx) => {
-  const { id } = ctx.params;
-
-  const post = posts.find(p => p.id.toString() === id);
-
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트 존재 안함'
-    };
-    return;
-  }
-  ctx.body = post;
 };
 
-// DELETE /api/posts/:id
 exports.remove = (ctx) => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트 존재 안함'
-    };
-    return;
-  }
-  posts.splice(index, 1);
-  ctx.status = 204; // No Content
 };
-
-// PUT /api/posts/:id
-exports.replace = (ctx) => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.bondy = {
-      message: '포스트 존재 안함'
-    };
-    return;
-  }
-  posts[index] = {
-    id,
-    ...ctx.request.body
-  };
-  ctx.body = posts[index];
-};
-
-// PATCH /api/posts/:id
 
 exports.update = (ctx) => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재 안함'
-    };
-    return;
-  }
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body
-  };
-  ctx.body = posts[index];
-}
+};
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
 const api = require('./api');
+
+const {
+  PORT: port = 4000,
+  MONGO_URI: mongoURI
+} = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, {useNuewUrlParser: true}).then(() => {
+  console.log('connected to mongodb');
+}).catch((e) => {
+  console.error(e);
+});
 
 const app = new Koa();
 const router = new Router();
@@ -13,6 +28,6 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('listening to port 4000');
+app.listen(port, () => {
+  console.log('listening to port', port);
 });
