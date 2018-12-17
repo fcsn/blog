d90c8a2e5674dc0432830da75527816f95b06391
sirfcsn <sirfcsn@gmail.com>
2018-12-17 15:08:32 +0900

    feat: Add tags query reading api in backend

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -43,35 +43,39 @@ exports.write = async (ctx) => {
 
 // GET /api/posts
 exports.list = async (ctx) => {
+  const page = parseInt(ctx.query.page || 1, 10);
+  const { tag } = ctx.query;
+
+  const query = tag ? {
+    tags: tag
+  } : {};
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
     // find() 함수를 호출한 후에는 exec()를 붙여 주어야 서버에 쿼리를 요청
     // 요청당 포스트 개수 제한 .limit
-    const page = parseInt(ctx.query.page || 1, 10);
-
-    if (page < 1) {
-      ctx.status = 400;
-      return;
-    }
-
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
 
-    const postCount = await Post.count().exec();
+    const postCount = await Post.count(query).exec();
 
     // 200자 이상이면 body 다음에 ... 보이게
     const limitBodyLength = post => ({
       ...post,
-      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+      body: post.body.length < 350 ? post.body : `${post.body.slice(0, 350)}...`
     });
     ctx.body = posts.map(limitBodyLength);
 
     // ctx.set은 response header 설정
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts;
   } catch (e) {
     ctx.throw(e, 500);
   }
