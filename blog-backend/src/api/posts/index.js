527cbbf805733706b234dda914e04fafd6d7bfde
sirfcsn <sirfcsn@gmail.com>
2018-12-27 21:32:29 +0900

    feat: Add checkLogin ctrl in posts.ctrl.js

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -6,9 +6,9 @@ const posts = new Router();
 // 라우트 처리 함수만 모아 놓은 파일을 컨트롤러
 // 컨트롤러에서는 백엔드 기능을 구현
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', postsCtrl.checkLogin, postsCtrl.write);
 posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.delete('/:id', postsCtrl.checkLogin, postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkLogin, postsCtrl.checkObjectId, postsCtrl.update);
 
 module.exports = posts;
