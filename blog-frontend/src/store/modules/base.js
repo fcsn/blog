730c473ed39619e13f232ddbc70c57db73532514
sirfcsn <sirfcsn@gmail.com>
2018-12-27 22:09:28 +0900

    feat: Add login and check login reducer in base.js

--- blog-frontend/src/store/modules/base.js
+++ blog-frontend/src/store/modules/base.js
@@ -3,16 +3,33 @@ import { createAction, handleActions } from 'redux-actions';
 import { Map } from 'immutable';
 import { pender } from 'redux-pender';
 
+import * as api from 'lib/api';
+
 const SHOW_MODAL = 'base/SHOW_MODAL';
 const HIDE_MODAL = 'base/HIDE_MODAL';
+const LOGIN = 'base/LOGIN';
+const LOGOUT = 'base/LOGOUT';
+const CHECK_LOGIN ='base/CHECK_LOGIN';
+const CHANGE_PASSWORD_INPUT = 'base/CHANGE_PASSWORD_INPUT';
+const INITIALIZE_LOGIN_MODAL = 'base/INITIALIZE_LOGIN_MODAL';
 
 export const showModal = createAction(SHOW_MODAL);
 export const hideModal = createAction(HIDE_MODAL);
+export const login = createAction(LOGIN, api.login);
+export const logout = createAction(LOGOUT, api.logout);
+export const checkLogin = createAction(CHECK_LOGIN, api.checkLogin);
+export const changePasswordInput = createAction(CHANGE_PASSWORD_INPUT);
+export const initializeLoginModal = createAction(INITIALIZE_LOGIN_MODAL);
 
-const ininitalState = Map({
+const initialState = Map({
     modal: Map({
         remove: false,
-        login: false
+        login: false,
+        loginModal: Map({
+                    password: '',
+                    error: false
+                }),
+        logged: false
     })
 });
 
@@ -24,5 +41,30 @@ export default handleActions({
     [HIDE_MODAL]: (state, action) => {
         const { payload: modalName } = action;
         return state.setIn(['modal', modalName], false);
-    }
-}, ininitalState)
+    },
+    ...pender({
+        type: LOGIN,
+        onSuccess: (state, action) => { // 로그인에 성공할 때
+            return state.set('logged', true);
+        },
+        onError: (state, action) => { // 오류가 발생할 때
+            return state.setIn(['loginModal', 'error'], true)
+        .setIn(['loginModal', 'password'], '');
+        }
+    }),
+    ...pender({
+        type: CHECK_LOGIN,
+        onSuccess: (state, action) => {
+            const { logged } = action.payload.data;
+            return state.set('logged', logged);
+        }
+    }),
+    [CHANGE_PASSWORD_INPUT]: (state, action) => {
+        const { payload: value } = action;
+        return state.setIn(['loginModal', 'password'], value);
+    },
+    [INITIALIZE_LOGIN_MODAL]: (state, action) => {
+        // 로그인 모달의 상태를 초기 상태로 설정(텍스트/오류 초기화)
+        return state.set('loginModal', initialState.get('loginModal'));
+    },
+}, initialState)
